ba8566ea0e9b95c6702bfb55285ab5df78345d54
shashi <>
2023-04-07 18:59:48 +0530

    animation added

--- diet-plan-app/src/component/slider/index.js
+++ diet-plan-app/src/component/slider/index.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
-import "./slider.css";
+import "../../assets/style/slider.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { detail } from "../../utils/const";
 import Fruits from "../../assets/gif/fruits.gif";
+import logo from "../../assets/logo.svg";
+import next from "../../assets/next.svg";
+import prev from "../../assets/prev.svg";
 
 const SlickSlider = () => {
+  const [value, setValue] = useState(0)
   var settings = {
     dots: true,
     infinite: false,
@@ -14,28 +18,59 @@ const SlickSlider = () => {
     slidesToScroll: 1,
   };
 
+  const handlePrev = () => {
+    setValue((prev) => prev - 1)
+  }
+  const handleNext = () => {
+    setValue((prev) => prev + 1)
+  }
   return (
-    <div className="container">
-      <Slider {...settings}>
-        {detail.map((item1, index1) => {
-          return (
-            <div key={index1}>
-              <h1>{item1.question}</h1>
-              <h3>{item1.description}</h3>
-              {item1.answers?.map((item2, index2) => {
+    <div className="container-fluid">
+      <img src={logo} alt="logo" />
+      <div className="slider-wraper">
+        <div className="container">
+          <>
+            {
+              detail.map((item, index) => {
                 return (
-                  <div key={index2}>
-                    <div>{item2.option}</div>
-                    <div>{item2.description}</div>
+                  <div className={`row align-items-center slides ${value === index ? "active-slide" : ""}`}>
+                    <div className="col-md-6">{value === index && <div className="slider-content animate__animated animate__backInUp">
+                      <h5 className="steps">Step 01</h5>
+                      <h2>{item.question}</h2>
+                      <h6 className="description">{item?.description}</h6>
+                      <div>{item?.answer_type === "input" && <input className="answer-input" placeholder="Type your answer here..." />}</div>
+                      {
+                        item.answers?.map((item2, index) => {
+                          return <h6 className="options"><span className="option">A</span>{item2.option}</h6>
+                        })
+                      }
+
+                      <button className="primary-solid">Next</button>
+                    </div>}
+                    </div>
+                    <div className="col-md-6">
+                      {<img width={800} height={600} src={Fruits} />}
+                    </div>
                   </div>
-                );
-              })}
-              {item1.answer_type === "input" && <input />}
-              {item1.id == 1 && <img height={100} width={200} src={Fruits} />}
-            </div>
-          );
-        })}
-      </Slider>
+                )
+              })
+            }
+          </>
+        </div>
+        <div className="navigation">
+          <button onClick={() => {
+            handlePrev()
+          }}>       <img src={next} alt="next" /></button>
+          <button onClick={() => {
+            handleNext()
+          }
+          }>
+
+            <img src={prev} alt="prev" />
+          </button>
+        </div>
+
+      </div >
     </div>
   );
 };
